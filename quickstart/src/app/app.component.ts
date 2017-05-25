d73842cb3b4294185538bc283765e8df7a0789f3
sanu <sanu.mn@pitsolutions.com>
2017-05-25 11:27:33 +0530

    get word from textbox and press enter to get output

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -4,42 +4,22 @@ import { Hero } from './hero';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <h2>My favorite hero is: {{myHero.name}}</h2>
-    <p>Heros:</p>
-    <ul>
-      <li *ngFor="let hero of heroes">
-        {{ hero.name }}
-      </li>
-    </ul>
-    <p *ngIf="heroes.length > 3">There are many heroes!</p>
-    <button (click)="onClickMe()">Click me!</button>
-    {{ clickMessage }}
-    <input (keyup)="onKey($event)">
-    <p>{{ values }}</p>
+    <input #newHero
+      (keyup.enter)="addHero(newHero.value)"
+      (blur)="addHero(newHero.value); newHero.value='' ">
 
+    <button (click)="addHero(newHero.value)">Add</button>
 
-    <input #box (keyup.enter)="onEnter(box.value)">
-    <p>{{box.value}}</p>
+    <ul><li *ngFor="let hero of heroes">{{hero}}</li></ul>
+
     `
 })
 
 export class AppComponent {
-  title = 'Tour of Heroes';
-  heroes = [
-  new Hero(1, 'Windstorm'),
-  new Hero(13, 'Bombasto'),
-  new Hero(15, 'Magneta'),
-  new Hero(20, 'Tornado')
-];
-  clickMessage = '';
-
-    onClickMe() {
-      this.clickMessage = 'You are my hero!';
+   heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  addHero(newHero: string) {
+    if (newHero) {
+      this.heroes.push(newHero);
     }
-
-  value = '';
-  onEnter(value: string) { this.value = value; }
-  
-  myHero = this.heroes[0];
+  }
 }
