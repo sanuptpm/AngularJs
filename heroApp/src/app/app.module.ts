6c2b7e039d87aaa5e4124b6009ee9b10b7ad53c3
sanu <sanu.mn@pitsolutions.com>
2017-06-02 15:08:31 +0530

    commit with hero app and muliptle component navigation

--- heroApp/src/app/app.module.ts
+++ heroApp/src/app/app.module.ts
@@ -1,21 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { AppComponent } from './app.component';
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent }     from './dashboard.component';
+import { HeroService }         from './hero.service';
+import { RouterModule }   from '@angular/router';
 
 @NgModule({
-  declarations: [
-  AppComponent,
-  HeroDetailComponent
-],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    RouterModule.forRoot([
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+  path: 'dashboard',
+  component: DashboardComponent
+},
+{
+  path: '',
+  redirectTo: '/dashboard',
+  pathMatch: 'full'
+},
+])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
