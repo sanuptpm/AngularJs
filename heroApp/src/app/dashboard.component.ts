7f32326ab1b6894cdf11a3c74f8db0ad769db9cf
sanu <sanu.mn@pitsolutions.com>
2017-06-07 17:05:41 +0530

    changes

--- heroApp/src/app/dashboard.component.ts
+++ heroApp/src/app/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+
 @Component({
   selector: 'my-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
@@ -13,3 +16,16 @@ export class DashboardComponent implements OnInit {
       .then(heroes => this.heroes = heroes.slice(1,5));
   }
 }
+
+
+
+// import { Component, OnInit } from '@angular/core';
+
+// import { Hero } from './hero';
+// import { HeroService } from './hero.service';
+
+// @Component({
+//   selector: 'my-dashboard',
+//   templateUrl: './dashboard.component.html',
+//   styleUrls: [ './dashboard.component.css' ]
+// })
