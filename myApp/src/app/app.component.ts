87f42e11f544c13624f7ccc7df8046af866216f3
sanu <sanu.mn@pitsolutions.com>
2017-06-07 16:54:48 +0530

    First angular app

---
+++ myApp/src/app/app.component.ts
@@ -0,0 +1,21 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'app works!';
+// }
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<login-form></login-form>'
+})
+export class AppComponent {
+  title = 'app works!';
+}
